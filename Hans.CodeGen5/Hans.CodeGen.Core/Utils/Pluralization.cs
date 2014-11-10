2263ba94e07ed53410a3e8edc71ecd92ebb01612
Muhamad Farihan <farihan@gmail.com>
2014-11-10 14:34:32 +0800

    remove comments and add pluralizatioin

---
+++ Hans.CodeGen5/Hans.CodeGen.Core/Utils/Pluralization.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Hans.CodeGen.Core.Utils
+{
+    public class Pluralization
+    {
+        private readonly IDictionary<string, string> Pluralizations = new Dictionary<string, string>
+        {
+            // Start with the rarest cases, and move to the most common
+            { "person", "people" },
+            { "ox", "oxen" },
+            { "child", "children" },
+            { "foot", "feet" },
+            { "tooth", "teeth" },
+            { "goose", "geese" },
+            // And now the more standard rules.
+            { "(.*)fe?", "$1ves" },         // ie, wolf, wife
+            { "(.*)man$", "$1men" },
+            { "(.+[aeiou]y)$", "$1s" },
+            { "(.+[^aeiou])y$", "$1ies" },
+            { "(.+z)$", "$1zes" },
+            { "([m|l])ouse$", "$1ice" },
+            { "(.+)(e|i)x$", @"$1ices"},    // ie, Matrix, Index
+            { "(octop|vir)us$", "$1i"},
+            { "(.+(s|x|sh|ch))$", @"$1es"},
+            { "(.+)", @"$1s" }
+        };
+
+        public string Pluralize(string word)
+        {
+            foreach (var pluralization in Pluralizations)
+            {
+                if (Regex.IsMatch(word, pluralization.Key))
+                {
+                    return Regex.Replace(word, pluralization.Key, pluralization.Value);
+                }
+            }
+
+            return word;
+        }
+    }
+}
